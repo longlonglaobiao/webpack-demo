cc0445903651cdf33175be13fb604da481565395
longlonglaobiao <2719330946@qq.com>
2020-02-07 21:39:21 +0800

    add .js .scss pictures files

--- src/modules/content.js
+++ src/modules/content.js
@@ -1,9 +1,14 @@
 import "../styles/content.scss";
+import img from "../assets/test.jpg";
+
 function content() {
   var rootDom = document.getElementById("root");
   var dom = document.createElement("div");
+  var mimg = new Image();
+  mimg.classList.add("timg");
+  mimg.src = img;
+  dom.appendChild(mimg);
   dom.setAttribute("id", "content");
-  dom.innerText = "this is content dom";
   rootDom.appendChild(dom);
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 module.exports = {
+  mode: "development",
   entry: "./src/index.js",
   output: {
     filename: "[name].js",
@@ -23,6 +24,17 @@ module.exports = {
           },
           "sass-loader"
         ]
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        use: {
+          loader: "url-loader",
+          options: {
+            limit: 20480, // 图片大小超过 20k 那么将图片放入 dist 目录，否则以 base64 编码形式写入生成的 js 文件中
+            outputPath: "imgs", // 设置图片的存放目录，当然超过 20k 的文件才存放
+            name: "[name]-[hash].[ext]" // 设置图片的名称
+          }
+        }
       }
     ]
   }
